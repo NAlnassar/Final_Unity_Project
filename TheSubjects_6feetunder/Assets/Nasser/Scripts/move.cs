104328f745cac0eae757c07df9ccb0217c2ffb99
LonelyNasser <105369522+NAlnassar@users.noreply.github.com>
2024-07-11 11:40:15 +0300

    AAdding Characters and animation

--- TheSubjects_6feetunder/Assets/Nasser/Scripts/move.cs
+++ TheSubjects_6feetunder/Assets/Nasser/Scripts/move.cs
@@ -15,13 +15,14 @@ public class move : MonoBehaviour, IPunObservable
 
     PhotonView view;
     GameObject active_ghost;
-
+    Animator anim;
     bool ability_active = false;
     Vector2 xRotation = Vector2.zero;
     public int ability = -1;
 
     Vector3 realposition = Vector3.zero;
     Quaternion realrotation = Quaternion.identity;
+    
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
@@ -29,17 +30,57 @@ public class move : MonoBehaviour, IPunObservable
         {
             stream.SendNext(transform.position);
             stream.SendNext(transform.rotation);
+            stream.SendNext(anim.GetFloat(anim.GetParameter(0).name));
+            stream.SendNext(anim.GetFloat(anim.GetParameter(1).name));
+            stream.SendNext(anim.GetFloat(anim.GetParameter(2).name));
+            stream.SendNext(anim.GetBool(anim.GetParameter(3).name));
+            stream.SendNext(anim.GetBool(anim.GetParameter(4).name));
+            stream.SendNext(anim.GetBool(anim.GetParameter(5).name));
+            stream.SendNext(anim.GetBool(anim.GetParameter(6).name));
+            stream.SendNext(anim.GetBool(anim.GetParameter(7).name));
+            stream.SendNext(anim.GetBool(anim.GetParameter(8).name));
+            stream.SendNext(anim.GetBool(anim.GetParameter(9).name));
+            stream.SendNext(anim.GetBool(anim.GetParameter(10).name));
+            stream.SendNext(anim.GetBool(anim.GetParameter(11).name));
+            stream.SendNext(anim.GetBool(anim.GetParameter(12).name));
+            stream.SendNext(anim.GetBool(anim.GetParameter(13).name));
+            
+
         }
         else
         {
+            //Fall-- 
+            //Drink--
+            //Ghost--
+            //Throw
+            //Happy
+            //WalkJump
+            //RunJump
             realposition = (Vector3)stream.ReceiveNext();
             realrotation = (Quaternion)stream.ReceiveNext();
+            anim.SetFloat(anim.GetParameter(0).name, (float)stream.ReceiveNext());//Hori
+            anim.SetFloat(anim.GetParameter(1).name, (float)stream.ReceiveNext());//Verti
+            anim.SetFloat(anim.GetParameter(2).name, (float)stream.ReceiveNext());//Blend
+            anim.SetBool(anim.GetParameter(3).name, (bool)stream.ReceiveNext());//Run
+            anim.SetBool(anim.GetParameter(4).name, (bool)stream.ReceiveNext()); //Jump
+            anim.SetBool(anim.GetParameter(5).name, (bool) stream.ReceiveNext());//Fall
+            anim.SetBool(anim.GetParameter(6).name, (bool)stream.ReceiveNext());//Climb
+            anim.SetBool(anim.GetParameter(7).name, (bool) stream.ReceiveNext());//Drink
+            anim.SetBool(anim.GetParameter(8).name, (bool) stream.ReceiveNext());//Ghost
+            anim.SetBool(anim.GetParameter(9).name, (bool)stream.ReceiveNext());
+            anim.SetBool(anim.GetParameter(10).name, (bool)stream.ReceiveNext());
+            anim.SetBool(anim.GetParameter(11).name, (bool)stream.ReceiveNext());
+            anim.SetBool(anim.GetParameter(12).name, (bool)stream.ReceiveNext());
+            anim.SetBool(anim.GetParameter(13).name, (bool)stream.ReceiveNext());
+
+
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         view = GetComponent<PhotonView>();
         body = GetComponent<Rigidbody>();
         if (view.IsMine)
