3a19baac17fef005bc18db0fcba0ecd63c0bfbe5
renad33 <44338279+renad33@users.noreply.github.com>
2024-07-12 02:49:39 +0300

    MindScene

--- TheSubjects_6feetunder/Assets/Scripts-RE/openDoor.cs
+++ TheSubjects_6feetunder/Assets/Scripts-RE/openDoor.cs
@@ -6,16 +6,30 @@ public class openDoor : MonoBehaviour
 {
     public Animator doorU;
     public Animator doorD;
+    public AudioManager audioManager;
 
-     void OnTriggerEnter(Collider other)
+    private bool player1InTrigger = false;
+    private bool player2InTrigger = false;
+
+    void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player2" || other.gameObject.tag == "Player1")
+        if (other.gameObject.tag == "Player1")
+        {
+            player1InTrigger = true;
+        }
+        else if (other.gameObject.tag == "Player2")
+        {
+            player2InTrigger = true;
+        }
+
+        if (player1InTrigger && player2InTrigger)
         {
             doorU.SetBool("open", true);
-            Debug.Log("Touched trigger");
             doorD.SetBool("open", true);
+            Debug.Log("Both players in trigger. Door opened.");
+            audioManager.PlayDoorOpen();
         }
     }
 
- 
+   
 }
