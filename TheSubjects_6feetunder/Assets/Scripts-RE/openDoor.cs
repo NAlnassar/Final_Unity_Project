446ccf9ac4373c8b19a64e02840cdb297f80d1d4
renad33 <44338279+renad33@users.noreply.github.com>
2024-07-15 14:45:05 +0300

    Door Animation In MindLab

--- TheSubjects_6feetunder/Assets/Scripts-RE/openDoor.cs
+++ TheSubjects_6feetunder/Assets/Scripts-RE/openDoor.cs
@@ -7,15 +7,44 @@ public class openDoor : MonoBehaviour
     public Animator doorU;
     public Animator doorD;
 
-     void OnTriggerEnter(Collider other)
+    private bool player1InTrigger = false;
+    private bool player2InTrigger = false;
+
+    void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player2" || other.gameObject.tag == "Player1")
+        if (other.gameObject.tag == "Player1")
+        {
+            player1InTrigger = true;
+        }
+        else if (other.gameObject.tag == "Player2")
+        {
+            player2InTrigger = true;
+        }
+
+        if (player1InTrigger && player2InTrigger)
         {
             doorU.SetBool("open", true);
-            Debug.Log("Touched trigger");
             doorD.SetBool("open", true);
+
         }
     }
 
- 
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player1")
+        {
+            player1InTrigger = false;
+        }
+        else if (other.gameObject.tag == "Player2")
+        {
+            player2InTrigger = false;
+        }
+
+        if (!player1InTrigger || !player2InTrigger)
+        {
+            doorU.SetBool("open", false);
+            doorD.SetBool("open", false);
+      
+        }
+    }
 }
