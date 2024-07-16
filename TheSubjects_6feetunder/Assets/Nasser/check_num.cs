c97f639ef616b263a64d83e26eb832853fab08fc
LonelyNasser <105369522+NAlnassar@users.noreply.github.com>
2024-07-16 14:03:12 +0300

    Update check_num.cs

--- TheSubjects_6feetunder/Assets/Nasser/check_num.cs
+++ TheSubjects_6feetunder/Assets/Nasser/check_num.cs
@@ -5,7 +5,7 @@ using Photon.Pun;
 
 public class check_num : MonoBehaviourPunCallbacks
 {
-    public int num_players = 0;
+    public static int num_players = 0;
     public Vector3[] spawn_points = new Vector3[2];
     public int num_Presses = 0;
     // Start is called before the first frame update
